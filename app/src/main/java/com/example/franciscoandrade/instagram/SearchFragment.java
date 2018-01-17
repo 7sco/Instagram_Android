2cebc8071ab9e0848c76d93e7aa7c776aa627fbb
7sco <xaviandrade14@gmail.com>
2018-01-17 15:29:45 -0500

    latest commit

--- app/src/main/java/com/example/franciscoandrade/instagram/SearchFragment.java
+++ app/src/main/java/com/example/franciscoandrade/instagram/SearchFragment.java
@@ -3,12 +3,9 @@ package com.example.franciscoandrade.instagram;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.example.franciscoandrade.instagram.restApi.model.Fileread;
 
 
 /**
@@ -33,7 +30,7 @@ public class SearchFragment extends Fragment {
 //        findApiToken();
 //        readFromFile(getActivity());
 
-        Log.d("TOKEN#", "onCreateView: "+ Fileread.getFile());
+        //Log.d("TOKEN#", "onCreateView: "+ Fileread.getFile());
 
         return v;
     }
